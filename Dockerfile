4ac32c03b0bee194663e3585cc739374c33e5bf8
Kaxxu <pawmentel@gmail.com>
2022-05-05 22:22:29 +0200

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,12 @@
-FROM alpine
+FROM openjdk
 
-WORKDIR ~/minecraft
+WORKDIR /opt/minecraft
+
 COPY server.jar .
 COPY server.properties .
 
-RUN apk update
-RUN apk add openjdk9
 RUN echo "eula=true" > eula.txt
 
 EXPOSE 25565
 
-CMD ["/bin/sh", "-l", "-c", "java -Xmx1024M -Xms1024M -jar ~/minecraft/server.jar"]
-
+CMD ["/bin/sh", "-l", "-c", "java -Xmx1024M -Xms1024M -jar /opt/minecraft/server.jar"]
--- build-run.sh
+++ build-run.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-NAME=${1:-mc_server}
-PORT=${2:-25565}
+NAME=${2:-mc_server}
+PORT=${3:-25565}
+
 API=https://launchermeta.mojang.com/mc/game/version_manifest.json
+VER_API=$(curl ${API} | jq .versions[0].url | sed 's/"//g')
+SERVER=$(curl ${VER_API} | jq .downloads.server.url | sed 's/"//g')
 
-VER_API=$(curl $API | jq .versions[0].url | sed 's/"//g')
-SERVER=$(curl $VER_API | jq .downloads.server.url | sed 's/"//g')
+function get() {
+  echo -e "\e[32mGet server.jar\e[0m"
+  wget ${SERVER}
+  echo ""
+}
 
-echo -e "\e[32mGet server.jar\e[0m"
-wget $SERVER
-echo ""
+function build() {
+  echo -e "\e[32mDocker Build\e[0m"
+  docker build -t ${NAME} .
+  echo ""
+}
 
-echo -e "\e[32mDocker Build\e[0m"
-docker build -t $NAME .
-echo ""
+function run() {
+  echo -e "\e[32mDocker Run\e[0m"
+  docker run -d -p 127.0.0.1:${PORT}:25565/udp ${NAME}
+  echo ""
+}
 
-echo -e "\e[32mDocker Run\e[0m"
-docker run -d -p $PORT:25565 $NAME
-echo ""
+function all() {
+  get
+  build
+  run
+}
+
+"$@"
