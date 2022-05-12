e7e41e5700648797e4dea8229a69412ca45f8d0c
Kaxxu <pawmentel@gmail.com>
2022-05-12 23:11:39 +0200

    feat: ecr create v3

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "minecraft" {
-  name                 = "minecraft"
+  name                 = var.name
   image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
